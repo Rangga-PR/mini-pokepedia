6f48f57a1334dcc25dbc85aa8e1ee4242a2946b4
Rangga-PR <ranggaputraraharja@gmail.com>
2021-01-23 20:15:59 +0700

    Update Hero Image test

--- src/components/common/HeroImage/HeroImage.spec.js
+++ src/components/common/HeroImage/HeroImage.spec.js
@@ -8,13 +8,20 @@ beforeEach(cleanup);
 describe('HeroImage component', () => {
   it('render with children', () => {
     const { getByText } = render(
-      <HeroImage
-        imgUrl="/static/image/pokemon.jpg"
-        width="100px"
-        height="100px">
-        <p>test</p>
-      </HeroImage>
+      <>
+        <HeroImage
+          imgUrl="/static/image/pokemon.jpg"
+          width="100px"
+          height="100px">
+          <p>test</p>
+        </HeroImage>
+
+        <HeroImage imgUrl="/static/image/pokemon.jpg">
+          <p>unspecified size test</p>
+        </HeroImage>
+      </>
     );
     expect(getByText('test')).toBeTruthy();
+    expect(getByText('unspecified size test')).toBeTruthy();
   });
 });

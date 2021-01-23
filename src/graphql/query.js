ee6ef549df34155dedb9733ba5170ca42eebb46e
Rangga-PR <ranggaputraraharja@gmail.com>
2021-01-23 21:32:11 +0700

    Add pokemons & pokemon query

---
+++ src/graphql/query.js
@@ -0,0 +1,33 @@
+import { gql } from '@apollo/client';
+
+export const GET_POKEMONS = gql`
+  query pokemons($limit: Int, $offset: Int) {
+    pokemons(limit: $limit, offset: $offset) {
+      next
+      previous
+      results {
+        id
+        name
+        image
+      }
+    }
+  }
+`;
+
+export const GET_POKEMON = gql`
+  query pokemon($name: String!) {
+    pokemon(name: $name) {
+      id
+      name
+      sprites
+      abilities
+      moves
+      stats
+      types
+      weight
+      height
+      location_area_encounters
+      species
+    }
+  }
+`;

1eaacb7fd00d5e1bf429a68f7ab1f74889f7203e
Rangga-PR <ranggaputraraharja@gmail.com>
2021-01-26 10:23:16 +0700

    Setup PWA

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+const withPlugins = require('next-compose-plugins');
+
+const config = {
   async rewrites() {
     return [
       {
@@ -11,3 +13,17 @@ module.exports = {
     domains: ['raw.githubusercontent.com'],
   },
 };
+
+if (process.env.NODE_ENV != 'test') {
+  const withPWA = require('next-pwa');
+  const runtimeCaching = require('next-pwa/cache');
+
+  config.pwa = {
+    dest: 'public',
+    runtimeCaching,
+    disable: process.env.NODE_ENV === 'development',
+  };
+  module.exports = withPlugins([withPWA], config);
+} else {
+  module.exports = config;
+}

c56548a640839639d0a7ab61235d7b79ff02322f
Rangga-PR <ranggaputraraharja@gmail.com>
2021-01-27 05:08:09 +0700

    Update create trainer test

--- src/components/CreateTrainer/CreateTrainer.spec.js
+++ src/components/CreateTrainer/CreateTrainer.spec.js
@@ -2,12 +2,19 @@ import { cleanup, fireEvent, render } from '@testing-library/react';
 
 import CreateTrainer from './CreateTrainer';
 import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 
 beforeEach(cleanup);
 
 describe('Create Trainer component', () => {
   it('able to create trainer', () => {
-    const { getByTestId } = render(<CreateTrainer />);
+    const history = createMemoryHistory();
+    const { getByTestId } = render(
+      <Router history={history}>
+        <CreateTrainer />
+      </Router>
+    );
 
     fireEvent.change(getByTestId('trainer-name'), { target: { value: 'red' } });
     fireEvent.click(getByTestId('create-button'));
